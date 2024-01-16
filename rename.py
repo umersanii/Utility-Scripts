b10fc5b7fecddad0ec7a7f2dd5d7a92239ceef6d
umersanii <iamumersani@gmail.com>
2024-01-17 03:38:47 +0500

    0.1.4.2

--- rename.py
+++ rename.py
@@ -4,19 +4,14 @@ c = 1
 
 def rename_files(directory):
     global c
-    # Change directory to the specified path
     os.chdir(directory)
 
-    # Iterate over files in the directory
     for filename in os.listdir(directory):
-            # Construct the new filename
             new_prefix = f"' ({c}).mp4"
 
-            # Rename the file
             os.rename(filename, new_prefix)
             print(f'Renamed: {filename} -> {new_prefix}')
             c = c+1
 
-# Example usage
-directory_path = r"C:\Users\iamum\Downloads\Zips\MassDownloader_editbyaxcp_user_saved_1_8_2024\New folder (2)"
+directory_path = r"path_to_folder_here"
 rename_files(directory_path)
